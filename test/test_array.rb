c3668a5bc83ef571a03ce1957eef63a0abf5f09e
Daisuke IKEGAMI <ikegami.da@gmail.com>
2011-03-28 23:33:19 +0900

    clean test/test_array.rb

--- test/test_array.rb
+++ test/test_array.rb
@@ -4,26 +4,10 @@ require 'rushcheck'
 class TC_RandomArray < Test::Unit::TestCase
 
   def setup
-    MyRandomArray.set_pattern(0) {|i| i}
+    MyRandomArray.set_pattern(Integer) {|i| i}
   end
 
   def teardown
-  end
-
-  class TestSPArray < RandomArray; end
-  def test_set_pattern
-    RushCheck::Assertion.new(Integer, Integer, Integer) do |x, i, n|
-      RushCheck::guard {i > 0}
-      TestSPArray.set_pattern(x) {|j| j}
-      a = TestSPArray.arbitrary.value(n, RushCheck::StdGen.new)
-      len = a.length
-      case len
-      when 0
-        a.empty? 
-      else 
-        RushCheck::Gen.choose(0, len - 1).bind {|i| (a.first == x && a[i]==i)}
-      end
-    end.quick_check
   end
 
   class MyRandomArray < RandomArray; end
