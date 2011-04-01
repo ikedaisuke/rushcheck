dfb9362ca56056b65d85c230dc38c965370c9d69
Daisuke IKEGAMI <ikegami.da@gmail.com>
2011-04-01 09:24:08 +0900

    fix another bug and clean in array.rb

--- lib/rushcheck/array.rb
+++ lib/rushcheck/array.rb
@@ -16,50 +16,40 @@ class RandomArray < Array
 
   # self.set_pattern must be executed before calling self.arbitrary.
   # The method defines pattern of random arrays for self.arbitrary.
-  # This takes a variable and a block, where the variable should be
-  # a class which is used for the first element of random array.
-  # On the other hand, the block should define random array by
-  # inductive way; the block takes two variables and the first
-  # variable is assumed as an array and the second variable is the
-  # index of array.
-  def self.set_pattern(base, &f)
-    @@base, @@indp = base, f
+  # This takes a block, where the variable should be a class which
+  # is used for the first element of random array.
+  #
+  # For example, the following code create a class of array which
+  # consists of random integers;
+  #
+  #     require 'rushcheck'
+  #
+  #     class IntegerRandomArray < RandomArray; end
+  #     # for all index i, components belong to Integer
+  #     IntegerRandomArray.set_pattern {|i| Integer}
+  #
+  #     RushCheck::Assertion.new(IntegerRandomArray) { |arr|
+  #       ... # test codes here
+  #     }
+  #
+  def self.set_pattern(&f)
+    @proc = f
     nil
   end
 
-  def self.create_components(cs, n, r)
-    case cs
-    when Class
-      cs.arbitrary.value(n, r)
-    when Array
-      cs.map {|c| self.create_components(c, n, r)}
-    else
-      err = [ "Unexpected arguments #{cs.inspect}.",
-              "Maybe forgotten calling set_pattern before?"
-            ].join(' ')
-      raise(RushCheck::InternalError::RushCheckError, err)
-    end
-  end
-
-  def self.generate_components(c, n, r)
-    c.arbitrary.generate(n, r)
-  end
-
-  # a private method for self.arbitrary
   def self.generate_array(len)
     RushCheck::Gen.new do |n, r|
-      ary = [self.generate_components(@@base, n, r)]
+      ary = []
       r2 = r
-      (1..len).each do |i|
+      (0..len).each do |i|
         r1, r2 = r2.split
-        ary << self.generate_components(@@indp.call(i), n, r1)
+        ary << @proc.call(i).arbitrary.value(n, r1)
       end
 
       ary
     end
   end
 
-  # a private method for self.arbitrary
   def self.arrange_len
     RushCheck::Gen.sized do |m|
       m = 1 - m if m <= 0
--- test/test_array.rb
+++ test/test_array.rb
@@ -1,12 +1,14 @@
 require 'test/unit'
-require 'rushcheck'
+require 'rushcheck/array'
 
 class TC_RandomArray < Test::Unit::TestCase
 
-  class MyRandomArray < NonEmptyRandomArray; end
+  class IntegerRandomArray < NonEmptyRandomArray; end
+  class StringRandomArray < NonEmptyRandomArray; end
 
   def setup
-    MyRandomArray.set_pattern(Integer) {|i| Integer}
+    IntegerRandomArray.set_pattern {|i| Integer}
+    StringRandomArray.set_pattern {|i| String}
   end
 
   def teardown
@@ -14,30 +16,46 @@ class TC_RandomArray < Test::Unit::TestCase
 
   def test_random_array_nonempty
     assert_equal true,
-    RushCheck::Assertion.new(MyRandomArray) { |xs|
-       ! xs.empty?
-    }.check
+      RushCheck::Assertion.new(IntegerRandomArray) { |xs|
+         ! xs.empty?
+      }.check
   end
 
-  def test_random_array_class
+  def test_random_array_class_integer
     results = []
-    RushCheck::Assertion.new(MyRandomArray) { |xs|
-      # each_with_index skips if xs is empty
-      xs.each_with_index { |x, i|
-        # x[i].class should be Fixnum or merely Bignum
-        results << (Integer == x[i].class.superclass)
-      }
-      results.all? {|r| r == true}
-    }.check
+    assert_equal true,
+      RushCheck::Assertion.new(IntegerRandomArray) { |xs|
+        # skips if xs is empty
+        xs.each { |x|
+          # x.class should be Fixnum or merely Bignum
+          results << (Integer == x.class.superclass)
+        }
+        results.all? {|r| r == true}
+      }.check
+  end
+
+  def test_random_array_class_string
+    results = []
+    assert_equal true,
+      RushCheck::Assertion.new(StringRandomArray) { |xs|
+        # skips if xs is empty
+        xs.each { |x|
+          # x.class should be String
+          results << (String == x.class)
+        }
+        results.all? {|r| r == true}
+      }.check
   end
 
   def test_arbitrary
-    assert_instance_of(RushCheck::Gen, MyRandomArray.arbitrary)
+    assert_instance_of(RushCheck::Gen,
+      IntegerRandomArray.arbitrary)
   end
 
   def test_coarbitrary
     g = RushCheck::Gen.unit(0)
-    assert_instance_of(RushCheck::Gen, MyRandomArray.new.coarbitrary(g))
+    assert_instance_of(RushCheck::Gen,
+     IntegerRandomArray.new.coarbitrary(g))
   end
 
 end
